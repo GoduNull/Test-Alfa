ac2f0b89216d771963c86cd52bf725716eb68e05
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-07-20 22:59:06 +0300

    feat: add item model

---
+++ src/Data/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Item
+    {
+        public string? Title { get; set; }
+        public string? Link { get; set; }  
+        public string? Description { get; set; }
+        public string? Category { get; set; } 
+        public DateTime PubDate { get; set; }
+    }
+}
