7451abd2f23040a70cdf4616d602f6e112afd998
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-07-24 16:07:18 +0300

    feat: add ReadManager

--- src/Data/Models/Item.cs
+++ src/Data/Models/Item.cs
@@ -12,6 +12,6 @@ namespace Data.Models
         public string? Link { get; set; }  
         public string? Description { get; set; }
         public string? Category { get; set; } 
-        public DateTime PubDate { get; set; }
+        public DateTime? PubDate { get; set; }
     }
 }
